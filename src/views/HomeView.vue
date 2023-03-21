7f985883fa04dfa8fc4bce9f80052bc4eec0fd99
khrystyna.stupak <khrystyna.rypetska@gmail.com>
2023-03-21 20:02:07 +0100

    add project to git

---
+++ src/views/HomeView.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="home">
+    <h1>{{ counterTitle }}</h1>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+const counterTitle = ref('My Counter')
+
+const increaseCounter = () => {
+  counter.value++
+}
+
+const decreaseCounter = () => {
+  counter.value--
+}
+</script>
+
+<!--
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    const increaseCounter = () => {
+      counter.value++
+    }
+
+    const decreaseCounter = () => {
+      counter.value--
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter,
+    }
+  }
+}
+</script>
+-->
+
+
+<!--
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    },
+  }
+}
+</script>
+-->
+
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
